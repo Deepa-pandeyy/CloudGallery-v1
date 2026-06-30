6fde9092e972ca99864c953ce8131790c500c37d
Deepa Pandey <deepapandey.9161@gmail.com>
2026-06-30 11:08:40 +0530

    Update backend URL for production

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://cloudgallery-v1-backend.onrender.com",
 });
 
 // Automatically attach JWT token
